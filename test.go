07cb198f54e454909e6edf4dc6166fc4fdcd0348
Arthit Kim <arthit@imake.co.th>
2021-06-20 00:59:19 +0700

    test deploy

--- test.go
+++ test.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func Authorize(w http.ResponseWriter, req *http.Request) {
+	// c, err := auth.New(ClientID, BaseURL+"/callback")
+	// if err != nil {
+	// 	fmt.Fprintf(w, "error:%v", err)
+	// 	return
+	// }
+	// http.SetCookie(w, &http.Cookie{Name: "state", Value: c.State, Expires: time.Now().Add(60 * time.Second)})
+
+	fmt.Fprintf(w, "auth:")
+}
+
+func Callback(w http.ResponseWriter, req *http.Request) {
+	// c, err := auth.New(ClientID, BaseURL+"/callback")
+	// if err != nil {
+	// 	fmt.Fprintf(w, "error:%v", err)
+	// 	return
+	// }
+	// http.SetCookie(w, &http.Cookie{Name: "state", Value: c.State, Expires: time.Now().Add(60 * time.Second)})
+
+	fmt.Fprintf(w, "callback:")
+}
 
 func main() {
 	fmt.Println("Hello, world.")
+	godotenv.Load()
+	port := os.Getenv("PORT")
+
+	router := mux.NewRouter()
+	headers := handlers.AllowedOrigins([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	methods := handlers.AllowedOrigins([]string{"GET", "POST", "DELETE", "PUT"})
+	origins := handlers.AllowedOrigins([]string{"*"})
+	router.HandleFunc("/auth", Authorize)
+	router.HandleFunc("/callback", Callback)
+	log.Fatal(http.ListenAndServe(":"+port, handlers.CORS(headers, methods, origins)(router)))
 }
