aecd2cc7df70cf05d99ef01e39d7f33934572be7
Arthit Kim <arthit@imake.co.th>
2021-06-20 14:53:46 +0700

    push

--- test.go
+++ test.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"github.com/utahta/go-linenotify"
 	"github.com/utahta/go-linenotify/auth"
+	"google.golang.org/api/iterator"
 )
 
 // EDIT THIS
@@ -20,6 +26,12 @@ var (
 	ClientSecret = "HJHgKSrqUuhIepCzNkCx7E82RSTN1m47dqPoS1Lf6VA"
 	// port         = ":9090"
 )
+
+type numsResponseData struct {
+	UserID string  `json:"UserID"`
+	Point  float64 `json:"Point"`
+	// Jsondata string `json:"Jsondata"`
+}
 
 func Authorize(w http.ResponseWriter, req *http.Request) {
 	c, err := auth.New(ClientID, BaseURL+"/callback")
@@ -41,6 +53,77 @@ func Callback(w http.ResponseWriter, req *http.Request) {
 	http.SetCookie(w, &http.Cookie{Name: "state", Value: c.State, Expires: time.Now().Add(60 * time.Second)})
 
 	fmt.Fprintf(w, "callback:")
+}
+
+func adddata(accessToken string) {
+	// Use the application default credentials
+	ctx := context.Background()
+	conf := &firebase.Config{ProjectID: "imake-flutter-firebase"}
+	app, err := firebase.NewApp(ctx, conf)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// getdocdata = "kim"
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
+
+	iter := client.Collection("tm_members_uat").OrderBy("issue_date", firestore.Desc).Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate: %v", err)
+		}
+		fmt.Println(doc.Ref.ID, "\n")
+		fmt.Println(doc.Data(), "\n")
+		// getdocdata = doc.Data()
+		// break
+		_, err = client.Collection("tm_members_uat").Doc(doc.Ref.ID).Set(ctx, map[string]interface{}{
+			"lineuserid": accessToken,
+		}, firestore.MergeAll)
+
+		if err != nil {
+			// Handle any errors in an appropriate way, such as returning them.
+			log.Printf("An error has occurred: %s", err)
+		}
+		break
+	}
+
+	// fmt.Println(getdocdata)
+	// doc := make(map[string]interface{})
+	// doc["name"] = "Hello Tokyo!"
+	// doc["country"] = "Japan"
+
+	// _, _, err = client.Collection("tm_members_uat").Add(ctx, doc)
+	// if err != nil {
+	// 	// Handle any errors in an appropriate way, such as returning them.
+	// 	log.Printf("An error has occurred: %s", err)
+	// }
+
+}
+
+func notificationtoline(response http.ResponseWriter, request *http.Request) {
+
+	decoder := json.NewDecoder(request.Body)
+
+	var numsData numsResponseData
+
+	decoder.Decode(&numsData)
+	fmt.Println("numsData")
+	fmt.Println(numsData)
+	fmt.Println(numsData.UserID)
+
+	token := numsData.UserID // EDIT THIS
+	msgtext := fmt.Sprintf("%s%.2f", "Your current point is ", numsData.Point)
+
+	c := linenotify.NewClient()
+	c.Notify(context.Background(), token, msgtext, "", "", nil)
 }
 
 func main() {
